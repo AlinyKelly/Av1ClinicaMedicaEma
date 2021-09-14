202efd54a4d1184a63f8c6f1913331759a7cb449
Aliny Kelly <alinykellyfs@gmail.com>
2021-09-14 20:52:45 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,3 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Fique atento, em breve um de nossos atendentes irá entrar em contato para confirmar sua consulta.</b>";
+}
